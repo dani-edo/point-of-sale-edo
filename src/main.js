eb4f182b0101ff53ee0accfa776e4523bd5a3f1e
dani.edo <dani.nurafandi@gomodo.tech>
2020-04-23 09:23:20 +0700

    vuetify init

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import firebase from "./plugins/firebase";
 import VueFirebase from "vue-firebase";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   VueFirebase,
   firebase,
-  render: (h) => h(App),
+  vuetify,
+  render: (h) => h(App)
 }).$mount("#app");

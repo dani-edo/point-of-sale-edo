4d6cf09bb5ae386815cfdf3e2faa77d37f65afcf
dani.edo <dani.nurafandi@gomodo.tech>
2020-04-23 09:19:14 +0700

    firebase init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import firebase from "./plugins/firebase";
+import VueFirebase from "vue-firebase";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  VueFirebase,
+  firebase,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/plugins/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCf_htznPTWraQQJCAfCwBn1x9T1BndoRo",
+  authDomain: "warung-si-mbok.firebaseapp.com",
+  databaseURL: "https://warung-si-mbok.firebaseio.com",
+  projectId: "warung-si-mbok",
+  storageBucket: "warung-si-mbok.appspot.com",
+  messagingSenderId: "217083199190",
+  appId: "1:217083199190:web:d2490538de28487b8b1377",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;

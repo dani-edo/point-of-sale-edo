90cb74f94ed26c1990bb5d398f105ef1240c5d08
dani.edo <dani.nurafandi@gomodo.tech>
2020-04-25 16:44:05 +0700

    edit product and validation

--- src/components/List.vue
+++ src/components/List.vue
@@ -22,7 +22,7 @@
             <div class="overline mt-1">{{ content.note }}</div>
           </v-list-item-content>
           <v-card-actions class="flex-column actions">
-            <v-btn variant="primary" class="primary">Edit</v-btn>
+            <v-btn variant="primary" class="primary" @click="editProduct(content.key)">Edit</v-btn>
             <v-btn variant="danger" class="error" @click="deleteProduct(content.key)">Del</v-btn>
           </v-card-actions>
         </v-list-item>
@@ -30,7 +30,7 @@
     </v-col>
 
     <!-- Modal create :start -->
-    <v-dialog v-model="modal_create" persistent max-width="600px">
+    <v-dialog v-model="condition.modal_create" persistent max-width="600px">
       <template v-slot:activator="{ on }">
         <!-- <v-btn color="primary" dark v-on="on">Open Dialog</v-btn> -->
         <v-btn class="floating-button" fab small dark color="primary" v-on="on">
@@ -48,27 +48,32 @@
               <v-row>
                 <v-col class="px-0 py-0" cols="12">
                   <v-text-field
+                    ref="name"
                     label="Nama"
                     hint="Nama produk harus jelas"
-                    v-model="data.name"
-                    required
+                    v-model="data_create.name"
+                    :rules="form_rules"
                   ></v-text-field>
                 </v-col>
                 <v-col class="px-0 py-0" cols="6">
                   <v-text-field
+                    ref="price"
                     label="Harga"
                     type="number"
-                    v-model="data.price"
+                    v-model="data_create.price"
                     hint="Tidak perlu ditambah titik atau koma"
+                    :rules="form_rules"
                     required
                   ></v-text-field>
                 </v-col>
                 <v-col class="px-0 py-0" cols="6">
                   <v-select
+                    ref="unit"
                     :items="['Buah', 'Kotak', 'Kilo', 'Ons']"
                     label="Satuan"
-                    v-model="data.unit"
+                    v-model="data_create.unit"
                     class="ml-1"
+                    :rules="form_rules"
                     required
                   ></v-select>
                 </v-col>
@@ -76,7 +81,7 @@
                   <v-text-field
                     label="Catatan (opsional)"
                     hint="Keterangan agar lebih jelas"
-                    v-model="data.note"
+                    v-model="data_create.note"
                   ></v-text-field>
                 </v-col>
               </v-row>
@@ -84,7 +89,7 @@
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="modal_create = false"
+            <v-btn color="blue darken-1" text @click="condition.modal_create = false"
               >Batal</v-btn
             >
             <v-btn color="blue darken-1" text type="submit">Simpan</v-btn>
@@ -96,13 +101,13 @@
 
     <!-- modal delete confirmation :start -->
     <v-dialog
-      v-model="modal_delete"
+      v-model="condition.modal_delete"
       max-width="290"
     >
       <v-card>
-        <v-card-title class="headline">?</v-card-title>
+        <!-- <v-card-title class="headline">?</v-card-title> -->
 
-        <v-card-text>
+        <v-card-text class="pt-5">
           Apakah anda yakin ingin menghapus produk ini?
         </v-card-text>
 
@@ -112,14 +117,14 @@
           <v-btn
             color="green darken-1"
             text
-            @click="modal_delete = false"
+            @click="condition.modal_delete = false"
           >
             Batal
           </v-btn>
 
-          <v-btn
-            color="green darken-1"
-            text
+          <v-btn 
+            variant="danger"
+            class="error"
             @click="deleteConfirmed()"
           >
             Hapus
@@ -141,15 +146,24 @@ export default {
     return {
       input: "",
       list: [],
-      modal_create: false,
-      modal_delete: false,
+      condition: {
+        modal_create: false,
+        modal_delete: false,
+        edit_product: false
+      },
+      key: {
+        edit_key: ''
+      },
       delete_confirmation_key: '',
-      data: {
+      data_create: {
         name: "",
         price: null,
         unit: "",
         note: "",
       },
+      form_rules: [
+        value => !!value || 'Required.'
+      ],
     };
   },
   mounted() {
@@ -175,24 +189,60 @@ export default {
       });
     },
     createFirebaseData() {
-      const firebaseRef = firebase.database().ref("/"),
-        firebaseKey = firebaseRef.push().key,
-        updates = {};
-      updates["/" + firebaseKey] = this.data;
-      firebaseRef.update(updates);
+      // Error check
+      var hasError = false;
+      Object.entries(this.$refs).forEach((e)=>{
+        if (e[1].hasError) {
+          hasError = true
+        }
+        return hasError
+      })
 
-      this.modal_create = false;
-      this.data.name = this.data.unit = this.data.note = "";
-      this.data.price = null;
+      if (!hasError) {
+        if (!this.condition.edit_product) { // <- Edit Product
+          var firebaseRef = firebase.database().ref("/"),
+            firebaseKey = firebaseRef.push().key,
+            updates = {};
+          
+        } else { // <- Create Product
+          firebaseRef = firebase.database().ref("/"),
+            firebaseKey = this.key.edit_key,
+            updates = {};
+        }
+        updates["/" + firebaseKey] = this.data_create;
+        firebaseRef.update(updates);
+        this.condition.modal_create = false;
+        this.condition.edit_product = false
+        this.emptyData()
+      } else {
+        Object.entries(this.$refs).forEach((e) => {
+          this.$refs[e[0]].validate(true)
+        })
+      }
+    },
+    editProduct(key) {
+      const firebaseRef = firebase.database().ref("/" + key);
+      firebaseRef.once('value', (snapshot) => {
+        const snap_val = snapshot.val()
+        this.data_create = snap_val
+        console.log(this.data_create)
+      })
+      this.condition.modal_create = true
+      this.condition.edit_product = true
+      this.key.edit_key = key
     },
     deleteProduct(key) {
-      this.modal_delete = true
+      this.condition.modal_delete = true
       this.delete_confirmation_key = key
     },
     deleteConfirmed() {
       firebase.database().ref("/" + this.delete_confirmation_key).remove()
       this.delete_confirmation_key = ''
-      this.modal_delete = false
+      this.condition.modal_delete = false
+    },
+    emptyData() {
+      this.data_create.name = this.data_create.unit = this.data_create.note = "";
+      this.data_create.price = null;
     }
   },
 };

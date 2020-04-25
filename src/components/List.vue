02d4b897732b4be7aad4714674bfc888d0cf36c2
dani.edo <dani.nurafandi@gomodo.tech>
2020-04-25 21:19:34 +0700

    upload and delete image

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,7 +2,7 @@
   <v-row>
     <v-col>
       <v-card
-        v-for="(content) in list"
+        v-for="content in list"
         :key="content.key"
         class="mx-auto product-list"
         max-width="100%"
@@ -10,7 +10,7 @@
       >
         <v-list-item three-line>
           <v-list-item-avatar tile size="80">
-            <v-img class="image-set" src=http://1.bp.blogspot.com/-113vEoo_OBk/UVuFWDlSHnI/AAAAAAAABoc/wB_y1ngKbD0/s1600/Simbok+Kerjo.jpg></v-img>
+            <v-img class="image-set" :src="content.imageUrl"></v-img>
           </v-list-item-avatar>
           <v-list-item-content>
             <v-list-item-title class="headline mb-1">{{
@@ -22,8 +22,18 @@
             <div class="overline mt-1">{{ content.note }}</div>
           </v-list-item-content>
           <v-card-actions class="flex-column actions">
-            <v-btn variant="primary" class="primary" @click="editProduct(content.key)">Edit</v-btn>
-            <v-btn variant="danger" class="error" @click="deleteProduct(content.key)">Del</v-btn>
+            <v-btn
+              variant="primary"
+              class="primary"
+              @click="editProduct(content.key)"
+              >Edit</v-btn
+            >
+            <v-btn
+              variant="danger"
+              class="error"
+              @click="deleteProduct(content.key, content.image_name)"
+              >Del</v-btn
+            >
           </v-card-actions>
         </v-list-item>
       </v-card>
@@ -52,7 +62,7 @@
                     label="Nama"
                     hint="Nama produk harus jelas"
                     v-model="data_create.name"
-                    :rules="form_rules"
+                    :rules="rules.form_rules"
                   ></v-text-field>
                 </v-col>
                 <v-col class="px-0 py-0" cols="6">
@@ -62,7 +72,7 @@
                     type="number"
                     v-model="data_create.price"
                     hint="Tidak perlu ditambah titik atau koma"
-                    :rules="form_rules"
+                    :rules="rules.form_rules"
                     required
                   ></v-text-field>
                 </v-col>
@@ -73,7 +83,7 @@
                     label="Satuan"
                     v-model="data_create.unit"
                     class="ml-1"
-                    :rules="form_rules"
+                    :rules="rules.form_rules"
                     required
                   ></v-select>
                 </v-col>
@@ -84,12 +94,26 @@
                     v-model="data_create.note"
                   ></v-text-field>
                 </v-col>
+                <v-col>
+                  <v-file-input
+                    ref="image"
+                    :rules="rules.image_rules"
+                    accept="image/png, image/jpeg, image/bmp"
+                    placeholder="Pick an avatar"
+                    prepend-icon="mdi-camera"
+                    label="Foto Produk"
+                    @change="handleImage"
+                  ></v-file-input>
+                </v-col>
               </v-row>
             </v-container>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="condition.modal_create = false"
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="condition.modal_create = false"
               >Batal</v-btn
             >
             <v-btn color="blue darken-1" text type="submit">Simpan</v-btn>
@@ -100,10 +124,7 @@
     <!-- Modal create :end -->
 
     <!-- modal delete confirmation :start -->
-    <v-dialog
-      v-model="condition.modal_delete"
-      max-width="290"
-    >
+    <v-dialog v-model="condition.modal_delete" max-width="290">
       <v-card>
         <!-- <v-card-title class="headline">?</v-card-title> -->
 
@@ -122,11 +143,7 @@
             Batal
           </v-btn>
 
-          <v-btn 
-            variant="danger"
-            class="error"
-            @click="deleteConfirmed()"
-          >
+          <v-btn variant="danger" class="error" @click="deleteConfirmed()">
             Hapus
           </v-btn>
         </v-card-actions>
@@ -139,43 +156,49 @@
 <script>
 import * as firebase from "firebase/app";
 import "firebase/database";
+import "firebase/storage";
 
 export default {
   name: "List",
   data() {
     return {
-      input: "",
+      imageInput: [],
       list: [],
       condition: {
         modal_create: false,
         modal_delete: false,
-        edit_product: false
+        edit_product: false,
       },
       key: {
-        edit_key: ''
+        edit_key: "",
+        delete_confirmation_key: "",
+        delete_image_name: "",
       },
-      delete_confirmation_key: '',
       data_create: {
         name: "",
         price: null,
         unit: "",
         note: "",
+        image_name: "",
       },
-      form_rules: [
-        value => !!value || 'Required.'
-      ],
+      rules: {
+        form_rules: [(value) => !!value || "Required."],
+        image_rules: [
+          (value) =>
+            !value ||
+            value.size < 2000000 ||
+            "Avatar size should be less than 2 MB!",
+        ],
+      },
     };
   },
   mounted() {
     this.getFirebaseData();
   },
   methods: {
-    onKeyup() {
-      console.log(this.input);
-    },
     getFirebaseData() {
-      const firebaseRef = firebase.database().ref("/");
-      firebaseRef.on("value", (snapshot) => {
+      const databaseRef = firebase.database().ref("/");
+      databaseRef.once("value", (snapshot) => {
         const snap_val = snapshot.val();
         this.list = [];
 
@@ -183,67 +206,123 @@ export default {
           Object.entries(snap_val).map((e) => {
             e[1].key = e[0];
             e[1].price = e[1].price.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-            this.list.push(e[1]);
+
+            firebase
+              .storage()
+              .ref("product_picture/" + e[1].image_name)
+              .getDownloadURL()
+              .then((data) => {
+                e[1].imageUrl = data;
+                this.list.push(e[1]);
+              })
+              .catch((error) => {
+                console.log("error", error);
+              });
           });
         }
       });
     },
+    handleImage(e) {
+      this.imageInput = e;
+    },
     createFirebaseData() {
       // Error check
       var hasError = false;
-      Object.entries(this.$refs).forEach((e)=>{
+      Object.entries(this.$refs).forEach((e) => {
         if (e[1].hasError) {
-          hasError = true
+          hasError = true;
         }
-        return hasError
-      })
+        return hasError;
+      });
 
       if (!hasError) {
-        if (!this.condition.edit_product) { // <- Edit Product
-          var firebaseRef = firebase.database().ref("/"),
-            firebaseKey = firebaseRef.push().key,
+        if (!this.condition.edit_product) {
+          // <- Edit Product
+          var databaseRef = firebase.database().ref("/"),
+            firebaseKey = databaseRef.push().key,
             updates = {};
-          
-        } else { // <- Create Product
-          firebaseRef = firebase.database().ref("/"),
-            firebaseKey = this.key.edit_key,
-            updates = {};
+        } else {
+          // <- Create Product
+          (databaseRef = firebase.database().ref("/")),
+            (firebaseKey = this.key.edit_key),
+            (updates = {});
         }
+        const image_name = firebaseKey + this.imageInput.name;
+        this.data_create.image_name = image_name;
         updates["/" + firebaseKey] = this.data_create;
-        firebaseRef.update(updates);
+        databaseRef.update(updates);
+
+        // Storage image upload :start
+        const storageRef = firebase
+          .storage()
+          .ref("product_picture/" + image_name);
+        const task = storageRef.put(this.imageInput);
+        task.on(
+          "state_changed",
+          (snapshot) => {
+            console.log(snapshot);
+            const percentage =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("uploadProgress", percentage);
+          },
+          (error) => {
+            console.log("error", error);
+          },
+          () => {
+            console.log("complete");
+            this.getFirebaseData();
+          }
+        );
+        // Storage image upload :end
+
         this.condition.modal_create = false;
-        this.condition.edit_product = false
-        this.emptyData()
+        this.condition.edit_product = false;
+        this.emptyData();
       } else {
         Object.entries(this.$refs).forEach((e) => {
-          this.$refs[e[0]].validate(true)
-        })
+          this.$refs[e[0]].validate(true);
+        });
       }
     },
     editProduct(key) {
-      const firebaseRef = firebase.database().ref("/" + key);
-      firebaseRef.once('value', (snapshot) => {
-        const snap_val = snapshot.val()
-        this.data_create = snap_val
-        console.log(this.data_create)
-      })
-      this.condition.modal_create = true
-      this.condition.edit_product = true
-      this.key.edit_key = key
+      const databaseRef = firebase.database().ref("/" + key);
+      databaseRef.once("value", (snapshot) => {
+        const snap_val = snapshot.val();
+        this.data_create = snap_val;
+      });
+      this.condition.modal_create = true;
+      this.condition.edit_product = true;
+      this.key.edit_key = key;
     },
-    deleteProduct(key) {
-      this.condition.modal_delete = true
-      this.delete_confirmation_key = key
+    deleteProduct(key, image) {
+      this.condition.modal_delete = true;
+      this.key.delete_confirmation_key = key;
+      this.key.delete_image_name = image;
     },
     deleteConfirmed() {
-      firebase.database().ref("/" + this.delete_confirmation_key).remove()
-      this.delete_confirmation_key = ''
-      this.condition.modal_delete = false
+      firebase
+        .database()
+        .ref("/" + this.key.delete_confirmation_key)
+        .remove();
+      firebase
+        .storage()
+        .ref("product_picture/" + this.key.delete_image_name)
+        .delete()
+        .then(() => {
+          console.log("deleted succesfully");
+          this.getFirebaseData();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      this.key.delete_confirmation_key = this.key.delete_image_name = "";
+      this.condition.modal_delete = false;
     },
     emptyData() {
-      this.data_create.name = this.data_create.unit = this.data_create.note = "";
+      this.data_create.name = this.data_create.unit = this.data_create.note =
+        "";
       this.data_create.price = null;
-    }
+    },
   },
 };
 </script>

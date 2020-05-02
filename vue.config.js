c76fb426e2e76c80b19ebec5953063325e7506a9
dani.edo <dani.nurafandi@gomodo.tech>
2020-05-03 05:24:53 +0700

    remove vue.config for pwa

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,19 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  pwa: {
-    name: 'warung-mbok-soinem',
-    themeColor: '#3bbcd4',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'white',
+  // pwa: {
+  //   name: 'warung-mbok-soinem',
+  //   themeColor: '#3bbcd4',
+  //   msTileColor: '#000000',
+  //   appleMobileWebAppCapable: 'yes',
+  //   appleMobileWebAppStatusBarStyle: 'white',
 
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
-  }
+  //   // configure the workbox plugin
+  //   workboxPluginMode: 'InjectManifest',
+  //   workboxOptions: {
+  //     // swSrc is required in InjectManifest mode.
+  //     swSrc: 'dev/sw.js',
+  //     // ...other Workbox options...
+  //   }
+  // }
 }
